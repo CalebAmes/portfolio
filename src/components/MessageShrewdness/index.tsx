2f2a8fbafcc1f85a6d712c44be7ab0dbf621e845
Caleb <clutchcaleb@gmail.com>
2022-03-13 18:30:33 -0400

    user messages come up on the right

--- src/components/MessageShrewdness/index.tsx
+++ src/components/MessageShrewdness/index.tsx
@@ -1,27 +1,42 @@
 import './MessageShrewdness.scss';
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 import { fetchMessages } from '../../utils/api';
 import { io } from "socket.io-client";
 
 const socket = io("https://shrewdness.herokuapp.com/")
 
+interface User {
+  username: string
+}
 interface MessageArray {
-  messageText: string
+  messageText: string,
+  userId: number,
+  User: User,
 }
 
 const MessageShrewdness = () => {
-  const [messages, setMessages] = useState<MessageArray[]>([])
-  const [message, setMessage] = useState<any>("")
+  const [messages, setMessages] = useState<MessageArray[]>([]);
+  const [message, setMessage] = useState<string>("");
+  const scrollRef = useRef<any>(null);
+
+
+  const scroll = () => {
+    if (scrollRef.current?.lastChild) {
+      scrollRef.current.lastChild.scrollIntoView({
+        behavior: "smooth"
+      })
+    }
+  }
 
   useEffect(() => {
-    socket.on(`chat_message_1`, () => {
-      fetchMessages().then(res => setMessages(() => res))
+    socket.on(`chat_message_1`, (msg) => {
+      fetchMessages().then(res => setMessages(() => res)).then(scroll)
     })
     socket.on(`edit_channel_1`, async () => {
       fetchMessages().then(res => setMessages(() => res))
     });
-    fetchMessages().then(res => setMessages(() => res))
-  }, [])
+    fetchMessages().then(res => setMessages(() => res)).then(scroll)
+  }, []);
 
   const inputHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
     setMessage(e.target.value)
@@ -50,22 +65,28 @@ const MessageShrewdness = () => {
 
   return (
     <div className='messageContainer'>
-      <div>MessageShrewdness</div>
-      {
-        messages.map((message, i) => {
-          return (
-            <div key={i} className="singleMessage">
-              <p>{message.messageText}</p>
-            </div>
-          )
-        })
-      }
+      <div className="messagesDiv" ref={scrollRef}>
+        {
+          messages.map((message, i) => {
+            let isDemoMessage = false;
+            if (message.userId === 1) isDemoMessage = true;
+            return (
+              <div key={i} style={isDemoMessage ? { alignSelf: "end", textAlign: "end" } : {}} className="singleMessage">
+                <p>{message.User.username}</p>
+                <h3>{message.messageText}</h3>
+              </div>
+            )
+          })
+        }
+      </div>
       <input
+        placeholder="press 'enter' to send"
         type="text"
         maxLength={140}
         onChange={inputHandler}
         onKeyPress={keyPress}
-        value={message} />
+        value={message}
+      />
     </div>
   )
 }
